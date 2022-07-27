b3e7db5b1112218afcbaff66edf0548caedafbc2
vayzenb <vayzenberg90@gmail.com>
2022-07-27 13:29:28 -0400

    udapted download scripts

--- download_func_data.py
+++ download_func_data.py
@@ -9,6 +9,8 @@ subs = ['hemispace1001','hemispace1002','hemispace1003', 'hemispace1004',
         'hemispace1005','hemispace1006','hemispace1007',
         'hemispace2001','hemispace2002','hemispace2003']
 
+subs = ['hemispace1005']
+
 tasks = ['loc','spaceloc','toolloc']
 copes =[[1,4, 10,12],[1,2,4,5],[1,2,3,4,6,7,8]]
 conds = [['face','word','face-scramble','word-scramble'],
@@ -26,11 +28,13 @@ for sub in subs:
 
     for task in enumerate(tasks):
         for cope in enumerate(copes[task[0]]):
+            try:
+                #copy func
+                bash_cmd = f'scp -r vayzenbe@mind.cs.cmu.edu:{sub_dir}/derivatives/fsl/{task[1]}/HighLevel_roi.gfeat/cope{cope[1]}.feat/stats/zstat1.nii.gz {local_dir}/{sub}/{sub}_{task[1]}_{conds[task[0]][cope[0]]}.nii.gz'
             
-            #copy func
-            bash_cmd = f'scp -r vayzenbe@mind.cs.cmu.edu:{sub_dir}/derivatives/fsl/{task[1]}/HighLevel_roi.gfeat/cope{cope[1]}.feat/stats/zstat1.nii.gz {local_dir}/{sub}/{sub}_{task[1]}_{conds[task[0]][cope[0]]}.nii.gz'
             
-            
-            subprocess.run(bash_cmd.split(),check = True)
+                subprocess.run(bash_cmd.split(),check = True)
+            except:
+                continue
     
 
